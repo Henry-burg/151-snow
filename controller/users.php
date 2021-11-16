5f2ec38f054f215264bbc3aa6449e96b585c8670
BURGAT Ba-Khanh-Henry <Ba-Khanh-Henry.BURGAT@cpnv.ch>
2021-11-16 10:36:05 +0100

    update

---
+++ controller/users.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file      users.php
+ * @brief     File description
+ * @author    Created by Ba-Khanh-Henry.BURGA
+ * @version   16.11.2021
+ */
+
+function login() {
+    require "view/login.php";
+}
--- index.php
+++ index.php
@@ -8,12 +8,16 @@
  */
 
 require "controller/navigation.php";
+require "controller/users.php";
 
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
     switch ($action) {
         case 'home' :
             home();
+            break;
+        case 'login' :
+            login();
             break;
         default :
             lost();
